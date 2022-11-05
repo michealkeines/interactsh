998d4a10744224e56a3badd1658efbd52f616bdf
Micheal Keines <keinesmicheal@gmail.com>
2022-11-05 10:00:50 -0400

    added decryption part

--- pkg/client/client.py
+++ pkg/client/client.py
@@ -1,4 +1,7 @@
 from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.Hash import SHA256
+from Cryptodome.Cipher import AES
 import requests
 import base64
 import uuid
@@ -73,9 +76,9 @@ class Client:
             print(e)
             print("request failed")
 
-    def check(self):
+    def check(self) -> dict:
         url = f'{self.server_url}/poll?id={self.correlation_id}&secret={self.secret_key}&check=test'
-
+        print(url)
         headers = {
             "Authorization": self.token
         }
@@ -88,12 +91,49 @@ class Client:
             )
             print(res.text)
             print("polled")
+            return res.json()
         except Exception as e:
             print(e)
             print("request failed")
+            return {"error": "request failed"}
+
+    def decrypt_response(self, value, key):
+        decoded_key = base64.b64decode(key)
+        priv_key = RSA.import_key(self.priv_key)
+        ciper = PKCS1_OAEP.new(
+            key=priv_key,
+            hashAlgo=SHA256,
+        )
+        
+        plain_key = ciper.decrypt(decoded_key)
+        print("plain plain_key")
+        print(plain_key)
+        # https://stackoverflow.com/questions/35811119/aes-encryption-golang-and-python
+        value = str(value)
+        # We add back the padding ("=") here so that the decode won't fail.
+        value = base64.b64decode(value + '=' * (4 - len(value) % 4), '-_')
+        iv, value = value[:AES.block_size], value[AES.block_size:]
+
+        # Python uses 8-bit segments by default for legacy reasons. In order to support
+        # languages that encrypt using 128-bit segments, without having to use data with
+        # a length divisible by 16, we need to pad and truncate the values.
+        remainder = len(value) % 16
+        padded_value = value + b'\0' * (16 - remainder) if remainder else value
+        cipher = AES.new(plain_key, AES.MODE_CFB, iv, segment_size=128)
+        # Return the decrypted string with the padding removed.
+        decoded =  cipher.decrypt(padded_value)[:len(value)]
+
+        print("decode final out")
+
+        try:
+            print(decoded.decode("utf-8"))
+        except Exception as e:
+            print(e)
+            print("decod failed")
+                 
 
 
-temp_client = Client(token="43ccfd520d55deaf1a037e6bafb577d5f41c93b38156870022b2d8c2120250ae")
+temp_client = Client(token="152d88c7c6b428fc55fa8ccbb3eab085b8a7a6aca8c101a7d3258fc3badecb72")
 
 temp_client.set_rsa_keys()
 
@@ -105,6 +145,13 @@ temp_client.register()
 try:
     while True:
         time.sleep(4)
-        temp_client.check()
+        out = temp_client.check()
+        if "error" in out:
+            print("no data in poll")
+            continue
+        temp_client.decrypt_response(
+            value=out["encrypted_data"],
+            key=out["key"]
+        )
 except KeyboardInterrupt:
     print("stopped polling")
